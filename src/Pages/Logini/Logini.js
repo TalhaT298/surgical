070558996187f7308a332ef14bdebaebbbda90be
Talha <mdtalha2008@gmail.com>
2023-03-07 04:18:38 +0600

    login/signUp

--- src/Pages/Logini/Logini.js
+++ src/Pages/Logini/Logini.js
@@ -1,59 +1,79 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import img from '../Home/Home/Untitled design (40).png';
-import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import img from "../Home/Home/Untitled-design-3--unscreen.gif";
+import { AuthContext } from "../../contexts/AuthProvider/AuthProvider";
 
 const Logini = () => {
+  const { login } = useContext(AuthContext);
 
-  const {login}=useContext(AuthContext);
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
 
-    const handleLogin =event =>{
-        event.preventDefault();
-        const form = event.target;
-        const email=form.email.value;
-        const password = form.password.value;
-
-        login(email, password)
-        .then(result => {
-            const user = result.user;
-            console.log(user);
-    })
-    .then(error => console.log(error))
-  }
-    return (
-        <div className="hero w-full my-20">
-  <div className="hero-content grid gap-24 md:grid-cols-2 flex-col lg:flex-row">
-    <div className="text-center lg:text-left">
-      
-     <img className= ''src={img} alt="" />
+    login(email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .then((error) => console.log(error));
+  };
+  return (
+    <div className="hero w-full my-20">
+      <div className="hero-content grid gap-24 md:grid-cols-2 flex-col lg:flex-row">
+        <div className="text-center lg:text-left">
+          <img className="" src={img} alt="" />
+        </div>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 py-20">
+          <from onSubmit={handleLogin} className="card-body">
+            <h1 className="text-5xl text-center font-bold text-cyan-900">
+              Login now
+            </h1>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text text-cyan-900">Email</span>
+              </label>
+              <input
+                type="text"
+                name="email"
+                placeholder="email"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text text-cyan-900">Password</span>
+              </label>
+              <input
+                type="text"
+                name="password"
+                placeholder="password"
+                className="input input-bordered"
+              />
+              <label className="label">
+                <a
+                  href="#"
+                  className="label-text-alt link link-hover text-cyan-900"
+                >
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <input className="btn bg-cyan-900 text-white btn-primary" type="submit" value="Login" />
+            </div>
+          </from>
+          <p className="text-center text-cyan-900">
+            New to EyeGlass{" "}
+            <Link className="text-orange-800 font-bold" to="/signup">
+              Sign Up
+            </Link>
+          </p>
+        </div>
+      </div>
     </div>
-    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 py-20">
-      <from onSubmit={handleLogin} className="card-body">
-      <h1 className="text-5xl text-center font-bold text-orange-200">Login now</h1>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text text-orange-200">Email</span>
-          </label>
-          <input type="text" name='email' placeholder="email" className="input input-bordered" />
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text text-orange-200">Password</span>
-          </label>
-          <input type="text" name='password' placeholder="password" className="input input-bordered" />
-          <label className="label">
-            <a href="#" className="label-text-alt link link-hover text-orange-200" >Forgot password?</a>
-          </label>
-        </div>
-        <div className="form-control mt-6">
-            <input className='btn btn-primary' type="submit" value="Login" />
-        </div>
-      </from>
-      <p className='text-center text-orange-200'>New to EyeGlass <Link className='text-orange-800 font-bold' to='/signup'>Sign Up</Link></p>
-    </div>
-  </div>
-</div>
-    );
+  );
 };
 
 export default Logini;
--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,64 +1,87 @@
-import React from 'react';
-import { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import img from '../Home/Home/Untitled design (40).png';
-import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
+import React from "react";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import img from "../Home/Home/Untitled-design-3--unscreen.gif";
+import { AuthContext } from "../../contexts/AuthProvider/AuthProvider";
 
 const SignUp = () => {
-    const {createUser}=useContext(AuthContext);
-    const handleSignUp =event =>{
-        event.preventDefault();
-        const form=event.target;
-        const email=form.email.value;
-        const password=form.password.value;
+  const { createUser } = useContext(AuthContext);
+  const handleSignUp = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
 
-        createUser(email,password)
-        .then(result => {
-            const user = result.user;
-            console.log(user);
-        })
-        .catch(err => console.error(err));
-    }
-    return (
-        <div className="hero w-full my-20 text-orange-200">
-        <div className="hero-content grid gap-24 md:grid-cols-2 flex-col lg:flex-row">
-          <div className="text-center lg:text-left">
-            
-           <img className= ''src={img} alt="" />
-          </div>
-          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 py-20">
-            <from onSubmit={handleSignUp} className="card-body">
+    createUser(email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((err) => console.error(err));
+  };
+  return (
+    <div className="hero w-full my-20 text-cyan-900">
+      <div className="hero-content grid gap-24 md:grid-cols-2 flex-col lg:flex-row">
+        <div className="text-center lg:text-left">
+          <img className="" src={img} alt="" />
+        </div>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 py-20">
+          <from onSubmit={handleSignUp} className="card-body">
             <h1 className="text-5xl text-center font-bold">Sign Up</h1>
-              <div className="form-control text-orange-200">
-                <label className="label">
-                  <span className="label-text text-orange-200">Name</span>
-                </label>
-                <input type="text" name='name' placeholder="Your Name" className="input input-bordered" />
-              </div>
+            <div className="form-control text-cyan-900">
+              <label className="label">
+                <span className="label-text text-cyan-900">Name</span>
+              </label>
+              <input
+                type="text"
+                name="name"
+                placeholder="Your Name"
+                className="input input-bordered"
+              />
+            </div>
 
-              <div className="form-control text-orange-200">
-                <label className="label">
-                  <span className="label-text text-orange-200">Email</span>
-                </label>
-                <input type="text" name='email' placeholder="email" className="input input-bordered" required/>
-              </div>
+            <div className="form-control text-cyan-900">
+              <label className="label">
+                <span className="label-text text-cyan-900">Email</span>
+              </label>
+              <input
+                type="text"
+                name="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
 
-              <div className="form-control text-orange-200">
-                <label className="label">
-                  <span className="label-text text-orange-200">Password</span>
-                </label>
-                <input type="text" name='password' placeholder="password" className="input input-bordered" />
-              </div>
-              <div className="form-control mt-6">
-                  <input className='btn btn-primary' type="submit" value="Sign Up" />
-              </div>
-            </from>
-            <p className='text-center'>Already Have an account? <Link className='text-orange-800 font-bold' to='/login'>Login</Link></p>
-          </div>
+            <div className="form-control text-cyan-900">
+              <label className="label">
+                <span className="label-text text-cyan-900">Password</span>
+              </label>
+              <input
+                type="text"
+                name="password"
+                placeholder="password"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control mt-6">
+              <input
+                className="btn btn-primary bg-cyan-900 text-white"
+                type="submit"
+                value="Sign Up"
+              />
+            </div>
+          </from>
+          <p className="text-center">
+            Already Have an account?{" "}
+            <Link className="text-orange-800 font-bold" to="/login">
+              Login
+            </Link>
+          </p>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default SignUp;
-
