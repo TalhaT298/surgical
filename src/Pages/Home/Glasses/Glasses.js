48c5fd9b9c28163300e4790312351697dd4aa538
Talha <mdtalha2008@gmail.com>
2024-07-27 20:47:50 +0600

    buy products done

--- src/Pages/Home/Glasses/Glasses.js
+++ src/Pages/Home/Glasses/Glasses.js
@@ -12,7 +12,17 @@ const Glasses = () => {
   }, []);
   return (
     <div className="py-10 pl-8">
-      <div className="flex justify-between">
+      <div className="text-center mb-6">
+          <h2 className="text-5xl font-bold text-cyan-700 mb-4">
+          Buy our products
+          </h2>
+        </div>
+        <div className="text-center mb-6 pb-10">
+          <h4 className="text-xl uppercase space-x-3 font-bold text-teal-600 mb-4">
+          Buy top-quality surgical products at great prices on our secure site with fast shipping and expert support!
+          </h4>
+        </div>
+      {/* <div className="flex justify-between">
         <figure>
           <img
             className="w-24 lg:w-48"
@@ -24,9 +34,9 @@ const Glasses = () => {
           <h4 className="lg:text-5xl md:text-2xl sm:text-3xl text-cyan-700 font-bold pt-10 pr-14">
             Buy our products
           </h4>
-          {/* <h2 className='text-4xl'>Buy our products</h2> */}
+          
         </div>
-      </div>
+      </div> */}
       <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {glasses.map((glass) => (
           <GlassCard key={glass._id} glass={glass}></GlassCard>
