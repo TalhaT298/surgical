8c1c6ab486f915093e7a6a525cb0eed47beeca2c
Talha <mdtalha2008@gmail.com>
2024-07-29 14:33:02 +0600

    footer mobile responsive

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -11,7 +11,7 @@
 //   return (
 //     <div className="">
 //       {/* B5D8DC */}
-//       {/* <Wave fill='rgb(0, 151, 167)' 
+//       {/* <Wave fill='rgb(0, 151, 167)'
 //         paused={false}
 //         options={{
 //           height: 80,
@@ -58,15 +58,15 @@
 //               </svg>{" "}
 //             </a>
 //             <a href="https://twitter.com/TalhaTarique1">
-//               <svg
-//                 xmlns="http://www.w3.org/2000/svg"
-//                 fill="currentColor"
-//                 width="24"
-//                 height="24"
-//                 viewBox="0 0 50 50"
-//               >
-//                 <path d="M 5.9199219 6 L 20.582031 27.375 L 6.2304688 44 L 9.4101562 44 L 21.986328 29.421875 L 31.986328 44 L 44 44 L 28.681641 21.669922 L 42.199219 6 L 39.029297 6 L 27.275391 19.617188 L 17.933594 6 L 5.9199219 6 z M 9.7167969 8 L 16.880859 8 L 40.203125 42 L 33.039062 42 L 9.7167969 8 z"></path>
-//               </svg>
+// <svg
+//   xmlns="http://www.w3.org/2000/svg"
+//   fill="currentColor"
+//   width="24"
+//   height="24"
+//   viewBox="0 0 50 50"
+// >
+//   <path d="M 5.9199219 6 L 20.582031 27.375 L 6.2304688 44 L 9.4101562 44 L 21.986328 29.421875 L 31.986328 44 L 44 44 L 28.681641 21.669922 L 42.199219 6 L 39.029297 6 L 27.275391 19.617188 L 17.933594 6 L 5.9199219 6 z M 9.7167969 8 L 16.880859 8 L 40.203125 42 L 33.039062 42 L 9.7167969 8 z"></path>
+// </svg>
 //             </a>
 //             <a href="https://www.instagram.com/talha_tarique77/">
 //               <svg
@@ -124,87 +124,86 @@
 
 // export default Footer;
 
-import React from 'react';
+import React from "react";
 import logo from "../../../assets/Untitled design (82).png";
 const Footer = () => {
   return (
     <div>
-      <footer className="footer bg-cyan-700 text-center justify-center gap-80 text-white text-sm  p-10">
-  <nav>
-    <h6 className="footer-title">Services</h6>
-    <a className="link link-hover">Branding</a>
-    <a className="link link-hover">Design</a>
-    <a className="link link-hover">Marketing</a>
-    <a className="link link-hover">Advertisement</a>
-  </nav>
-  <nav>
-    <h6 className="footer-title">Company</h6>
-    <a className="link link-hover">About us</a>
-    <a className="link link-hover">Contact</a>
-    <a className="link link-hover">Jobs</a>
-    <a className="link link-hover">Press kit</a>
-  </nav>
-  <nav>
-    <h6 className="footer-title">Legal</h6>
-    <a className="link link-hover">Terms of use</a>
-    <a className="link link-hover">Privacy policy</a>
-    <a className="link link-hover">Cookie policy</a>
-  </nav>
-  <nav>
-    <h6 className="footer-title">Privacy</h6>
-    <a className="link link-hover">Terms of use</a>
-    <a className="link link-hover">Privacy policy</a>
-    <a className="link link-hover">Cookie policy</a>
-  </nav>
-</footer>
-<footer className="footer bg-cyan-700 text-base-content border-base-300 border-t px-10 py-4">
-  <aside className="grid-flow-col items-center">
-  <img className='w-28' src={logo} alt="" />
-    <p className='text-white font-bold text-sm'>
-    © 2023 All right reserved by SurgicalSavvy
-      <br />
-      
-    </p>
-  </aside>
-  <nav className="md:place-self-center md:justify-self-end ">
-    <div className="grid grid-flow-col gap-4">
-      <a href=''>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          className="fill-current">
-          <path
-            d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
-        </svg>
-      </a>
-      <a href=''>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          className="fill-current">
-          <path
-            d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-        </svg>
-      </a>
-      <a href=''>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          className="fill-current">
-          <path
-            d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-        </svg>
-      </a>
-    </div>
-  </nav>
-</footer>
-    </div>
+  <footer className="footer bg-cyan-700 text-white text-sm p-10 flex flex-col md:flex-row md:justify-center md:gap-80">
+    <nav className="mb-6 md:mb-0">
+      <h6 className="footer-title">Services</h6>
+      <a className="link link-hover">Branding</a>
+      <a className="link link-hover">Design</a>
+      <a className="link link-hover">Marketing</a>
+      <a className="link link-hover">Advertisement</a>
+    </nav>
+    <nav className="mb-6 md:mb-0">
+      <h6 className="footer-title">Company</h6>
+      <a className="link link-hover">About us</a>
+      <a className="link link-hover">Contact</a>
+      <a className="link link-hover">Jobs</a>
+      <a className="link link-hover">Press kit</a>
+    </nav>
+    <nav className="mb-6 md:mb-0">
+      <h6 className="footer-title">Legal</h6>
+      <a className="link link-hover">Terms of use</a>
+      <a className="link link-hover">Privacy policy</a>
+      <a className="link link-hover">Cookie policy</a>
+    </nav>
+    <nav>
+      <h6 className="footer-title">Privacy</h6>
+      <a className="link link-hover">Terms of use</a>
+      <a className="link link-hover">Privacy policy</a>
+      <a className="link link-hover">Cookie policy</a>
+    </nav>
+  </footer>
+  <footer className="footer bg-cyan-700 text-base-content border-t border-base-300 px-10 py-4 flex flex-col md:flex-row items-center justify-between">
+    <aside className="flex items-center mb-4 md:mb-0">
+      <img className="w-28" src={logo} alt="SurgicalSavvy Logo" />
+      <p className="text-white font-bold text-sm ml-4">
+        © 2023 All rights reserved by SurgicalSavvy
+      </p>
+    </aside>
+    <nav className="flex justify-center md:justify-end">
+      <div className="flex gap-4">
+        <a href="">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="white"
+            width="24"
+            height="24"
+            viewBox="0 0 50 50"
+          >
+            <path d="M 5.9199219 6 L 20.582031 27.375 L 6.2304688 44 L 9.4101562 44 L 21.986328 29.421875 L 31.986328 44 L 44 44 L 28.681641 21.669922 L 42.199219 6 L 39.029297 6 L 27.275391 19.617188 L 17.933594 6 L 5.9199219 6 z M 9.7167969 8 L 16.880859 8 L 40.203125 42 L 33.039062 42 L 9.7167969 8 z"></path>
+          </svg>
+        </a>
+        <a href="">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="white"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+          >
+            <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
+          </svg>
+        </a>
+        <a href="">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="white"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+          >
+            <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
+          </svg>
+        </a>
+      </div>
+    </nav>
+  </footer>
+</div>
+
   );
 };
 
