deffa36e87af4016f76ef017d31c4e1f1f99d946
Talha <mdtalha2008@gmail.com>
2024-07-30 23:43:30 +0600

    social media link add at footer

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -166,7 +166,7 @@ const Footer = () => {
     </aside>
     <nav className="flex justify-center md:justify-end">
       <div className="flex gap-4">
-        <a href="">
+        <a href="https://www.instagram.com/talha_tarique77/">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="white"
@@ -177,7 +177,7 @@ const Footer = () => {
             <path d="M 5.9199219 6 L 20.582031 27.375 L 6.2304688 44 L 9.4101562 44 L 21.986328 29.421875 L 31.986328 44 L 44 44 L 28.681641 21.669922 L 42.199219 6 L 39.029297 6 L 27.275391 19.617188 L 17.933594 6 L 5.9199219 6 z M 9.7167969 8 L 16.880859 8 L 40.203125 42 L 33.039062 42 L 9.7167969 8 z"></path>
           </svg>
         </a>
-        <a href="">
+        <a href="https://twitter.com/TalhaTarique1">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="white"
@@ -188,7 +188,7 @@ const Footer = () => {
             <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
           </svg>
         </a>
-        <a href="">
+        <a href="https://www.facebook.com/talha.tarique.5">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="white"
